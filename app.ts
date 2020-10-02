0f1acb13bd9fbb7dbe9ae3fbd354a890c0328753
vansoundz <evannivan@gmail.com>
2020-10-02 21:12:51 +0300

    setup aws

--- app.ts
+++ app.ts
@@ -15,36 +15,45 @@ const PORT = process.env.PORT || 5000;
 
 app.use(Express.json());
 
-// (async () => {
-const MONGO_URI = process.env.MONGO_URI!;
-// try {
-mongoose.connection.once("open", () => console.log("Connected to db"));
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+(async () => {
+  const MONGO_URI = process.env.MONGO_URI!;
+  try {
+    mongoose.connection.once("open", () => console.log("Connected to db"));
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-// app.use(
-//   "/gql",
-//   graphqlHTTP({
-//     schema,
-//     graphiql: true,
-//   })
-// );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
+    app.use(Express.static("static"));
+
+    app.use(`/products`, products);
+    app.use(`/users`, users);
+    app.use(`/orders`, orders);
+
+    app.get(`/`, (req: Request, res: Response) => {
+      res.send(Date.now());
+    });
+
+    const server = app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+
+    server.on("error", (err) => console.log(err));
+    server.on("close", () => server.close());
+
+    process.on("uncaughtException", () => server.close());
+
+    // app.use(
+    //   "/gql",
+    //   graphqlHTTP({
+    //     schema,
+    //     graphiql: true,
+    //   })
+    // );
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 // app.use(`/upload`, fileUpload);
-app.use(Express.static("static"));
-
-app.get(`/`, (req: Request, res: Response) => {
-  res.send(Date.now());
-});
-app.use(`/products`, products);
-app.use(`/users`, users);
-app.use(`/orders`, orders);
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
