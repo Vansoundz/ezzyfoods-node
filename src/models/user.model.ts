4180f30721660a47ec265aea41e42b43407258c7
vansoundz <thebeatplanet.silente@gmail.com>
2020-10-30 11:44:03 +0300

    added admin

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   phone: { type: String },
   password: { type: String },
+  isAdmin: { type: Boolean, default: false },
   products: [
     {
       type: Types.ObjectId,
